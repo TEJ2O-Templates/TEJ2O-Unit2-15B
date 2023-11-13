2dbb614cba194e256a5f1c5b33df45721432c66a
Patrick Coxall <mr.coxall@mths.ca>
2023-11-13 09:58:25 -0500

    update

--- main.ts
+++ main.ts
@@ -7,7 +7,8 @@
 
 // variables
 let sprite: game.LedSprite = null
-let loopCounter = 0
+let loopCounter1 = 0
+let loopCounter2 = 0
 
 // setup
 basic.showIcon(IconNames.Happy)
@@ -18,32 +19,22 @@ basic.clearScreen()
 input.onButtonPressed(Button.A, function () {
     // setup
     basic.clearScreen()
-    loopCounter = 0
+    loopCounter1 = 0
+    loopCounter2 = 0
     sprite = game.createSprite(0, 0)
+    basic.pause(500)
 
-    while (loopCounter <= 5) {
-        sprite.set(LedSpriteProperty.X, loopCounter)
-        sprite.set(LedSpriteProperty.Y, loopCounter)
-        loopCounter = loopCounter + 1
+    while (loopCounter1 <= 4) {
+      while (loopCounter2 <= 4) {
+        sprite.move(1)
+        loopCounter1 = loopCounter1 + 1
         basic.pause(500)
+      }
+      loopCounter2++
+      loopCounter1 = 0
+      sprite.turn(Direction.Right, 90)
     }
-    sprite.delete()
-    basic.showIcon(IconNames.Happy)
-})
 
-// when "B" is pressed, the pixels move around the outside edge of the LED matrix
-input.onButtonPressed(Button.B, function () {
-    // setup
-    basic.clearScreen()
-    loopCounter = 5
-    sprite = game.createSprite(5, 5)
-
-    while (loopCounter >= 0) {
-        sprite.set(LedSpriteProperty.X, loopCounter)
-        sprite.set(LedSpriteProperty.Y, loopCounter)
-        loopCounter = loopCounter - 1
-        basic.pause(500)
-    }
     sprite.delete()
     basic.showIcon(IconNames.Happy)
 })
